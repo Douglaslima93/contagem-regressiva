51f55544d80b9d259fe0927f48b3b3069ee946ba
Douglas Lima <douglas07a@gmail.com>
2023-05-04 16:50:26 -0300

    Atulização v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Title from './component/title'
+import Counter from './component/Counter'
+import NewYear from './assets/img/ano-novo.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-       
-    </>
+    <div className='App' style={{backgroundImage: 'url({NewYear})'}}>
+      <div className='container'>
+        <Title title = "contagem regressiva"/>
+        <div className='contagem-container'>
+          <Counter title = "Dias" number = {2}/>
+          <Counter title = "Horas" number = {2}/>
+          <Counter title = "Minutos" number = {2}/>
+          <Counter title = "Segundos" number = {2}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/component/Title.jsx
@@ -0,0 +1,9 @@
+import react from 'react'
+
+const Title = ({title}) => {
+    return(
+        <div>Contagem Regressiva</div>
+    )
+}
+
+export default Title
