6ad1de4a46275c777bca1c165bc3b3b4f3e2510a
Douglas Lima <douglas07a@gmail.com>
2023-05-05 11:20:17 -0300

    Contador finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,21 @@ import Counter from './component/Counter'
 import NewYear from './assets/img/ano-novo.jpg'
 import './App.css'
 
+import useCountdown from './hooks/useCountdown,jsx'
+
 function App() {
+
+  const [day, hour, minute, second] = useCountdown('jan 1, 2024 00:00:00')
+
   return (
     <div className='App' style={{backgroundImage: `url(${NewYear})`}}>
       <div className='container'>
-        <Title title = "contagem regressiva"/>
+        <Title title = "contagem regressiva para 2024!!!"/>
         <div className='contagem-container'>
-          <Counter title = "Dias" number = {2}/>
-          <Counter title = "Horas" number = {2}/>
-          <Counter title = "Minutos" number = {2}/>
-          <Counter title = "Segundos" number = {2}/>
+          <Counter title = "Dias" number = {day}/>
+          <Counter title = "Horas" number = {hour}/>
+          <Counter title = "Minutos" number = {minute}/>
+          <Counter title = "Segundos" number = {second}/>
         </div>
       </div>
     </div>
