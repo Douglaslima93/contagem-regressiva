100b454952eb1d779af454a39d6ab8fdf254f6c8
Douglas Lima <douglas07a@gmail.com>
2023-05-08 11:56:06 -0300

    Aplicação responsiva

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ import useCountdown from './hooks/useCountdown,jsx'
 
 function App() {
 
-  const [day, hour, minute, second] = useCountdown('jan 1, 2024 00:00:00')
+const [day, hour, minute, second] = useCountdown(' jan 1, 2024 00:00:00')
+
 
   return (
     <div className='App' style={{backgroundImage: `url(${NewYear})`}}>
       <div className='container'>
-        <Title title = "contagem regressiva para 2024!!!"/>
+        <Title title = "Contagem regressiva para 2024!!!"/>
         <div className='contagem-container'>
           <Counter title = "Dias" number = {day}/>
           <Counter title = "Horas" number = {hour}/>
